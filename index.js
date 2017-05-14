d792cc5005a4b39f21c1e4bba6e2400d838f72d9
kfoerster <kevin.foerster@interone.de>
2017-05-14 20:05:30 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const Nightmare = require('nightmare');
+const exec = require('sync-exec');
+require('nightmare-inline-download')(Nightmare);
+
+function puts(error, stdout, stderr) {
+	sys.puts(stdout)
+}
+// TODO: username should come from keychain as well
+const username = exec(`security find-generic-password -gs Vodafone | grep '"acct"<blob>=' | cut -d'=' -f 2 | sed -e 's/^"//' -e 's/"$//'`).stdout.trim()
+const password = exec(`security find-generic-password -ws Vodafone`).stdout;
+const downloadDir = exec(`echo $HOME`).stdout.trim() + '/Downloads/';
+
+const nightmare = Nightmare({
+	show: false,
+	paths: {
+		downloads: downloadDir
+	}
+});
+
+var downloadInfo = nightmare
+	.goto('https://kabel.vodafone.de/customerSSO/UI/Login?goto=https://kabel.vodafone.de/meinkabel/rechnungen/rechnung')
+	.type('input#IDToken1', username)
+	.type('input#IDToken2', password)
+	.click('.btn--submit')
+	.wait('div.accordion')
+	.evaluate(function() {
+		var buttons = document.querySelectorAll("a[href^='/meinkabel/rechnungen/rechnung_download']");
+		return Array.prototype.map.call(buttons, function(e) {
+			return e.getAttribute('href')
+		});
+	})
+	
+	.then((urls) => {
+		// only get the latest invoice
+		console.log('will download invoices to: ' + downloadDir);
+		var url = urls[0];
+			nightmare
+				.click("a[href^='" + url.trim() + "']")
+				.download()
+		return nightmare.end();
+	})
+
+	.catch(function(error) {
+		console.error('failed:', error);
+	});
